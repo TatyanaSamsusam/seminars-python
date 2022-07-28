bb3a5219fa0e45a8a09c959362c2adc7f1c3ef62
Tatyana <android87@list.ru>
2022-07-28 15:16:44 +0400

    sort the list in file

--- sem4/task2.py
+++ sem4/task2.py
@@ -1,10 +1,29 @@
 # 2. Создать и заполнить файл случайными целыми значениями. Выполнить сортировку содержимого файла по возрастанию. 
 # (не использовать sort и sorted)
-
+import random
 from random import randint
 
+list_of_nums = []
+for i in range (1, 20):
+    list_of_nums.append(random.randint(1,10))
+
 with open('text2.txt', mode = 'w') as file:            # каждый раз записывает новые рандомные индексы в файл text.txt
-    for i in range(10):
-        file.write(f"\n{randint(0, 10)}")
+    for i in list_of_nums:
+        file.write(f"{i}\n")
+file.close()
 
 with open('text2.txt', mode = 'r') as file:
+    data = file.read()
+
+list_of_rows = data.split()
+for i in range(0, len(list_of_rows)):
+    list_of_rows[i] = int(list_of_rows[i])
+
+def sort_the_list(some_list):
+    for i in range(0, len(some_list)):
+        for j in range(i+1, len(some_list)):
+            if some_list[j] < some_list[i]:
+                some_list[j], some_list[i] = some_list[i], some_list[j]
+print(list_of_nums)
+sort_the_list(list_of_rows)
+print(list_of_rows)
